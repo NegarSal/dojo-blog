17e064b6a60022ec55e261703967df0969eac86a
Negar <negarsaljooghi@gmail.com>
2024-05-08 11:12:14 +0330

    Programmatic Redirects

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
     const [isPending, setIsPending] = useState(false)
+    const navigate = useNavigate();
 
     const handleSubmit = e => {
         e.preventDefault()
@@ -19,40 +21,41 @@ const Create = () => {
         }).then(() => {
             console.log('new blog added')
             setIsPending(false)
+            navigate("/")
         })
     }
 
-    return (
-        <div className="create">
-            <h2>Add a New Blog</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Blog title:</label>
-                <input
-                    type="text"
-                    required
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                />
-                <label>Blog body:</label>
-                <textarea
-                    required
-                    value={body}
-                    onChange={e => setBody(e.target.value)}
-                ></textarea>
-                <label>Blog author:</label>
-                <select
-                    value={author}
-                    onChange={e => setAuthor(e.target.value)}
-                >
-                    <option value="mario">mario</option>
-                    <option value="yoshi">yoshi</option>
-                </select>
-                {!isPending && <button>Add Blog</button>}
-                {isPending && <button disabled>Adding Blog...</button>}
+return (
+    <div className="create">
+        <h2>Add a New Blog</h2>
+        <form onSubmit={handleSubmit}>
+            <label>Blog title:</label>
+            <input
+                type="text"
+                required
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+            <label>Blog body:</label>
+            <textarea
+                required
+                value={body}
+                onChange={e => setBody(e.target.value)}
+            ></textarea>
+            <label>Blog author:</label>
+            <select
+                value={author}
+                onChange={e => setAuthor(e.target.value)}
+            >
+                <option value="mario">mario</option>
+                <option value="yoshi">yoshi</option>
+            </select>
+            {!isPending && <button>Add Blog</button>}
+            {isPending && <button disabled>Adding Blog...</button>}
 
-            </form>
-        </div>
-    );
+        </form>
+    </div>
+);
 }
 
 export default Create;
